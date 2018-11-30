550715b10f6ca39202f87080f595cc2de2c1afd5
mhd-rawashdah <mhd.rawashdah@gmail.com>
2018-11-30 19:54:25 +0200

    (init)

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+app.use(express.static(__dirname + '/../client/build'));
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
