98202e66910e3c68ba9f54731872bc3d46dd6893
radwan-1 <radwanbara@gmail.com>
2018-12-02 14:49:49 +0200

    (feat)App Component and style

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import './style/App.css';
+import video from './style/cooking.mp4';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Advertising Craft App</h1>
-        </header>
+        <div className="container center">
+          <nav className="menu">
+            <h1 className="menu__logo">adCraft</h1>
+              <div className="menu__right">
+                <ul className="menu__list">
+                    <li className="menu__list-item"><a className="menu__link" href="#"><i className="fa fa-facebook fa-lg"></i></a></li>
+                    <li className="menu__list-item"><a className="menu__link" href="#"><i className="fa fa-instagram fa-lg"></i></a></li>
+                    <li className="menu__list-item"><a className="menu__link" href="#"><i className="fa fa-twitter fa-lg"></i></a></li>
+                    
+                    <li className="menu__list-item"><a className="menu__link" href="#">Login</a></li>
+                    <li className="menu__list-item"><a className="menu__link" href="#">SIGN UP</a></li>
+                </ul>
+             </div>
+          </nav>
+          <div className="video-container">
+           <video width="300" height="200" loop autoPlay>
+              <source src={video} type="video/mp4" />
+                </video>
+              <div className="content">
+           <button id="myBtn">USER</button>
+           <button id="myBtn1">ADVERTISER</button>
+		</div>
       </div>
+    </div>
     );
   }
 }
