f63c8075d3b0c8eb77356f029d4ae8e244783d2c
Qusay97 <jacob900097@gmail.com>
2018-12-02 16:19:26 +0200

    (test) adding test for database to check on connection and insert data

--- server/index.js
+++ server/index.js
@@ -11,31 +11,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// to get date format
-let formatDate = () => {
-	var d = new Date();
-	 dformat = [d.getMonth()+1,
-               d.getDate(),
-               d.getFullYear()].join('-')+' '+
-               [d.getHours(),
-               d.getMinutes(),
-               d.getSeconds()].join(':');
-     return dformat;         
-}
-
-var user ={
-	firstName: "MOhammad",
-	lastName: "hasan",
-	phoneNumber: "0796305984",
-	password: "helloworld",
-	gender: "male",
-	id_roles: 2,
-	createdAt : formatDate()
-}
-
-db.insertUser(user,function(err, res){
-
-})
-
 
 app.listen(port, ()  => console.log(`Listening on port ${port}`));
