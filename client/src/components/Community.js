970126c41da115ade0ea95f869db024838ad8124
radwan-1 <radwanbara@gmail.com>
2018-12-08 14:06:21 +0200

    (feat) final component addition and finalize testing

---
+++ client/src/components/Community.js
@@ -0,0 +1,164 @@
+// { /******************************************* Community Contribution Section *********************************/}
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../style/App.css';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import GridListTileBar from '@material-ui/core/GridListTileBar'
+import logo from '../style/signup.png';
+import border from '../style/border.png';
+import radwan from '../cohort5/radwan.jpg';
+import mohammad from '../cohort5/mohammad_rawashdeh.jpg';
+import qusay from '../cohort5/qusai.jpg';
+import ezraa from '../style/ezraa.jpg';
+import ashraf from '../cohort5/ashraf.jpg';
+import ashraf1 from '../cohort5/ashraf1.jpg';
+import azhar from '../cohort5/azhar.jpg';
+import besslan from '../cohort5/besslan.jpg';
+import firas from '../cohort5/firas.jpg';
+import ghazi from '../cohort5/ghazi.jpg';
+import issa from '../cohort5/issa.jpg';
+import jozaa from '../cohort5/jozaa.jpg';
+import mahmoud from '../cohort5/mahmoud.jpg';
+import mahmoud2 from '../cohort5/mahmoud2.jpg';
+import mansour from '../cohort5/mansour.jpg';
+import mohannad from '../cohort5/mohannad.jpg';
+import mustafa from '../cohort5/mustafa.jpg';
+import noel from '../cohort5/noel.jpg';
+import omar from '../cohort5/omar.jpg';
+import yahye from '../cohort5/yahye.jpg';
+import yazeed from '../cohort5/yazeed.jpg';
+import ahmad from '../cohort5/ahmad.jpg';
+
+
+// static type component but can be change as a preference
+class Community extends Component {
+        render () {
+            return (
+            <Grid className="ninjaScript1" container direction={'row'}>
+                {/* // Top View */}
+                <Grid item xs={1} >
+                <img src={mohammad} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>               
+                <Grid  item xs={1} >
+                <img src={ahmad} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={radwan} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={qusay} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid item xs={1} >
+                <img src={ashraf} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>               
+                <Grid  item xs={1} >
+                <img src={ashraf1} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={azhar} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={besslan} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid item xs={1} >
+                <img src={firas} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>               
+                <Grid  item xs={1} >
+                <img src={ghazi} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={issa} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid item xs={1} >
+                <img src={jozaa} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                
+                {/* // Middle View */}
+                <Grid item xs={1} >
+                <img src={mahmoud} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "80px"}}/>
+                </Grid>               
+                <Grid  item xs={1} >
+                <img src={mahmoud2} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "80px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={mansour} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "80px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={mohannad} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "80px"}}/>
+                </Grid>
+                <Grid item xs={1} >
+                <img src={mustafa} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "80px"}}/>
+                </Grid>               
+                <Grid  item xs={1} >
+                <img src={noel} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "80px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={omar} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "80px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={yahye} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "80px"}}/>
+                </Grid>
+                <Grid item xs={1} >
+                <img src={yazeed} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "80px"}}/>
+                </Grid>               
+                <Grid  item xs={1} >
+                <img src={radwan} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "80px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={mustafa} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "80px"}}/>
+                </Grid>
+                <Grid item xs={1} >
+                <img src={mohammad} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "80px"}}/>
+                </Grid>               
+                <Grid  item xs={1} >
+                <img src={ahmad} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={radwan} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={qusay} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid item xs={1} >
+                <img src={mohammad} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>               
+                <Grid  item xs={1} >
+                <img src={ahmad} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={radwan} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={qusay} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid item xs={1} >
+                <img src={mohammad} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>               
+                <Grid  item xs={1} >
+                <img src={ahmad} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid  item xs={1} >
+                <img src={radwan} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid item xs={1} >
+                <img src={qusay} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                <Grid item xs={1} >
+                <img src={yahye} className="media" height="100" width="100" alt="" style={{"borderRadius":"50px", marginLeft: "150px"}}/>
+                </Grid>
+                
+            </Grid>
+            )
+        }
+}
+
+export default Community;
